c18fd12211615a60dff74fb2edf4bef4006951d6
fran <franromano03@gmail.com>
2025-11-10 18:57:53 -0300

    Avance en la creacion de clases:     - Creadas las clases Dado, Tablero, Hexagono y recursos     - Faltan modelar las aristas y los vertices y conectarlos correctamente       con los hexagonos

---
+++ src/main/java/edu/fiuba/algo3/modelo/Hexagono.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.Random;
+
+public class Hexagono {
+    private Recurso recurso;
+    private int numero;
+
+    public Hexagono(Recurso recurso, int numero) {
+        this.recurso = recurso;
+        this.numero = numero;
+    }
+
+    public Recurso VerificarNumero(int numero) {
+        if (numero == this.numero)
+            return this.recurso;
+        else
+            return null;
+    }
+
+}
