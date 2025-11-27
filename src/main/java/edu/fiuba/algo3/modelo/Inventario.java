ba1a149de693b98ea7232c9a6bb4fd63e912817d
Ellukemati <dundic.matias@hotmail.com>
2025-11-27 18:53:24 -0300

    Implementado comercio con la Banca influenciado por Puertos. Tests y refactors pertinentes.

--- src/main/java/edu/fiuba/algo3/modelo/Inventario.java
+++ src/main/java/edu/fiuba/algo3/modelo/Inventario.java
@@ -18,8 +18,20 @@ public class Inventario {
         this.recursos.compute(recurso, (k, cantidadActual) -> cantidadActual + cantidad);
     }
 
+    public void agregar(Map<Recurso, Integer> recursosNuevos) {
+        for (Map.Entry<Recurso, Integer> entry : recursosNuevos.entrySet()) {
+            this.agregar(entry.getKey(), entry.getValue());
+        }
+    }
+
     public void quitar(Recurso recurso, int cantidad) {
         this.recursos.compute(recurso, (k, cantidadActual) -> Math.max(0, cantidadActual - cantidad));
+    }
+
+    public void quitar(Map<Recurso, Integer> recursosAQuitar) {
+        for (Map.Entry<Recurso, Integer> entry : recursosAQuitar.entrySet()) {
+            this.quitar(entry.getKey(), entry.getValue());
+        }
     }
 
     public int cantidadTotalDeRecursos() {
@@ -46,7 +58,57 @@ public class Inventario {
         return true;
     }
 
-    public Map<Recurso, Integer> obtenerCopia() {
-        return new HashMap<>(this.recursos);
+    public void canjear(Recurso recursoAEntregar, int costo, Recurso recursoARecibir, int ganancia) {
+        if (this.cantidadDe(recursoAEntregar) >= costo) {
+            this.quitar(recursoAEntregar, costo);
+            this.agregar(recursoARecibir, ganancia);
+        }
+    }
+
+    public void realizarTransferencia(Map<Recurso, Integer> salida, Map<Recurso, Integer> entrada) {
+        this.quitar(salida);
+        this.agregar(entrada);
+    }
+
+    public void descartarMitad() {
+        int total = this.cantidadTotalDeRecursos();
+        if (total <= 7) return;
+
+        int cantidadADescartar = total / 2;
+        int acumuladorDescartados = 0;
+
+        // PRIMERA IMPLEMENTACIÓN: Descarta los primeros X recursos que encuentra
+        for (Recurso recurso : Recurso.values()) {
+            int cantidadActual = this.recursos.get(recurso);
+
+            if (cantidadActual > 0) {
+                int cantidadAQuitarDeEsteRecurso = Math.min(cantidadActual, cantidadADescartar - acumuladorDescartados);
+                this.quitar(recurso, cantidadAQuitarDeEsteRecurso);
+                acumuladorDescartados += cantidadAQuitarDeEsteRecurso;
+            }
+
+            if (acumuladorDescartados == cantidadADescartar) break;
+        }
+    }
+
+    public Recurso extraerRecursoAlAzar() {
+        int totalCartas = this.cantidadTotalDeRecursos();
+        if (totalCartas == 0) return null;
+
+        int indiceRandom = new java.util.Random().nextInt(totalCartas);
+        int acumulado = 0;
+
+        for (Recurso recurso : Recurso.values()) {
+            int cantidad = this.recursos.get(recurso);
+
+            if (cantidad > 0) {
+                acumulado += cantidad;
+                if (indiceRandom < acumulado) {
+                    this.quitar(recurso, 1);
+                    return recurso;
+                }
+            }
+        }
+        return null;
     }
 }
